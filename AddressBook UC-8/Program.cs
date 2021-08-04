a511ba85030c01f46b416958aa258fa27de501c4
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-08-04 23:15:30 +0530

    [Guru]Add:Search a Person Name Using City in Address Book

--- AddressBook UC-8/Program.cs
+++ AddressBook UC-8/Program.cs
@@ -13,7 +13,7 @@ namespace AddressBook_UC_8
             listPersonsInCity.Add(new Person("Raj", "Ram", "134.sdf", "7896541235", "Kochi", "Kerala", "621654", "qwe@gmail.com"));
             listPersonsInCity.Add(new Person("Vijay", "Kamal", "143,ghj.", "9584235874", "Bangalore", "Karnataka", "635541", "bac@gmail.com"));
 
-            Console.WriteLine("Find a Name of a Person in City Salem");
+            Console.WriteLine("Find a Name of a Person in City Salem ");
             foreach (Person person in listPersonsInCity.FindAll(e => (e.City == "Salem")).Take(1).ToList())
             {
                 Console.WriteLine("Name : " + person.FirstName);
